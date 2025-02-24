0c924f98ad11cb7a714f85470b4156245b6c910b
Rooby-Roo <thegreenrobby@gmail.com>
2025-02-24 15:05:41 -0700

    combat names first pass

---
+++ worlds/frogmonster/names/combat_names.py
@@ -0,0 +1,43 @@
+groth = "Groth"
+marvin = "Marvin"
+snake = "The Long Beast"
+yanoy = "Yanoy"
+placeholder = "Placeholder"
+limbs = "Limbs"
+eels = "Bog and Chog"
+xoto = "Xoto"
+valda = "Valda"
+djumbo = "Djumbo"
+dekula = "Dekula"
+chroma = "Chroma"
+tymbal = "Tymbal"
+foraz = "Foraz"
+hedgeward = "Hedgeward"
+runi = "Runi"
+barge = "Barge"
+door_crab = "Door Crab"
+lazaro = "Lazaro"
+zythida = "Zythida"
+krogar = "Krogar and the Scavengers"
+moridono = "Moridono"
+brothers = "Oosk and Dord"
+myzand_1 = "Myzand 1"
+myzand_2 = "Myzand 2"
+
+outskirts_arena_1 = "Outskirts Arena 1"  # Actually both of them - Hard
+yellow_forest_arena_1 = "Yellow Forest Arena 1"  # To Town - Hard
+yellow_forest_arena_2 = "Yellow Forest Arena 2"  # To Marvin's - Very Hard
+green_sea_arena_1 = "Green Sea Arena 1"  # Hard
+green_sea_arena_2 = "Green Sea Arena 2"  # Hard
+old_road_general = "Old Road General Combat"  # Hard
+old_road_arena_1 = "Old Road Arena 1"  # After first Save - Very Hard
+old_road_arena_2 = "Old Road Arena 2"  # Immediately before Valda - Very Hard
+under_city_arena_1 = "Under City Arena 1"  # Campfire + Scorpions - Normal
+forest_floor_arena_1 = "Forest Floor Arena 1"  # Toros before Chroma - Hard
+old_wood_general = "Old Wood General Combat"  # Hard
+hive_general = "Hive General Combat"  # Very Hard
+
+# Easy: Analogous item pickups
+# Normal: Some restrictions
+# Hard: What I can handle
+# Very Hard: What I can't handle
