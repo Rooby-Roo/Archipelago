1231badb50b7373f79c091539478ccf34a19e45e
RoobyRoo <thegreenrobby@gmail.com>
2025-03-04 11:03:59 -0700

    removing get_custom_rules for direct import

--- worlds/frogmonster/options.py
+++ worlds/frogmonster/options.py
@@ -4,11 +4,11 @@ from Options import Toggle, Range, Choice, PerGameCommonOptions, StartInventoryP
 class GameDifficulty(Choice):
     """Determines expected player skill. A harder difficulty means you will be expected to go further in the game with less resources."""
     display_name = "Game Difficulty"
-    option_easy = 1
-    option_normal = 2
+#    option_easy = 1
+#    option_normal = 2
     option_hard = 3
-    option_very_hard = 4
-    default = 2
+#    option_very_hard = 4
+    default = 3
 
 class ExcludePuzzles(Toggle):
     """When enabled, puzzles and their rewards will be excluded. This includes the chest in the Estate pool."""
