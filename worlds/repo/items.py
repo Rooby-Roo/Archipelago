c25636471d2e13ebb9d117d383738bea02b3ee4a
Devin <superdevin00@gmail.com>
2025-06-21 15:08:01 -0500

    Added shop items to item pool

--- worlds/repo/items.py
+++ worlds/repo/items.py
@@ -17,6 +17,8 @@ class REPOItemData(NamedTuple):
 
 
 # a lot of quantities here will need to be adjusted later
+
+#Last used number - 59
 item_table: Dict[str, REPOItemData] = {
     
     # ---- LEVELS ----
@@ -37,6 +39,47 @@ item_table: Dict[str, REPOItemData] = {
     iname.tumble_up: REPOItemData(ItemClassification.filler,1,17,"Upgrades"),
     iname.crouch_rest: REPOItemData(ItemClassification.filler,1,18,"Upgrades"),
     iname.tumble_wings: REPOItemData(ItemClassification.filler,1,19,"Upgrades"),
+
+    # ---- SHOP UNLOCKS ----
+    iname.small_health: REPOItemData(ItemClassification.filler,1,21,"Health Pack"),
+    iname.medium_health: REPOItemData(ItemClassification.filler,1,22,"Health Pack"),
+    iname.large_health: REPOItemData(ItemClassification.filler,1,23,"Health Pack"),
+    iname.progressive_health: REPOItemData(ItemClassification.filler,1,24,"Shop Unlock"),
+    iname.baseball_bat: REPOItemData(ItemClassification.filler,1,25,"Melee Shop Unlock"),
+    iname.frying_pan: REPOItemData(ItemClassification.filler,1,26,"Melee Shop Unlock"),
+    iname.sledge_hammer: REPOItemData(ItemClassification.filler,1,27,"Melee Shop Unlock"),
+    iname.sword: REPOItemData(ItemClassification.filler,1,28,"Melee Shop Unlock"),
+    iname.inflatable_hammer: REPOItemData(ItemClassification.filler,1,29,"Melee Shop Unlock"),
+    iname.prodzap: REPOItemData(ItemClassification.filler,1,30,"Melee Shop Unlock"),
+    iname.gun: REPOItemData(ItemClassification.filler,1,31,"Ranged Shop Unlock"),
+    iname.shotgun: REPOItemData(ItemClassification.filler,1,32,"Ranged Shop Unlock"),
+    iname.tranq_gun: REPOItemData(ItemClassification.filler,1,33,"Ranged Shop Unlock"),
+    iname.pulse_pistol: REPOItemData(ItemClassification.filler,1,34,"Ranged Shop Unlock"),
+    iname.photon_blaster: REPOItemData(ItemClassification.filler,1,35,"Ranged Shop Unlock"),
+    iname.boltzap: REPOItemData(ItemClassification.filler,1,36,"Ranged Shop Unlock"),
+    iname.cart_cannon: REPOItemData(ItemClassification.filler,1,37,"Ranged Shop Unlock"),
+    iname.cart_laser: REPOItemData(ItemClassification.filler,1,38,"Ranged Shop Unlock"),
+    iname.grenade: REPOItemData(ItemClassification.filler,1,39,"Explosive Shop Unlock"),
+    iname.shock_grenade: REPOItemData(ItemClassification.filler,1,40,"Explosive Shop Unlock"),
+    iname.stun_grenade: REPOItemData(ItemClassification.filler,1,41,"Explosive Shop Unlock"),
+    iname.duct_taped_grenade: REPOItemData(ItemClassification.filler,1,42,"Explosive Shop Unlock"),
+    iname.shockwave_mine: REPOItemData(ItemClassification.filler,1,43,"Explosive Shop Unlock"),
+    iname.stun_mine: REPOItemData(ItemClassification.filler,1,44,"Explosive Shop Unlock"),
+    iname.explosive_mine: REPOItemData(ItemClassification.filler,1,45,"Explosive Shop Unlock"),
+    iname.rubber_duck: REPOItemData(ItemClassification.filler,1,46,"Explosive Shop Unlock"),
+    iname.recharge_drone: REPOItemData(ItemClassification.filler,1,47,"Drone Shop Unlock"),
+    iname.indestructible_drone: REPOItemData(ItemClassification.filler,1,48,"Drone Shop Unlock"),
+    iname.roll_drone: REPOItemData(ItemClassification.filler,1,49,"Drone Shop Unlock"),
+    iname.feather_drone: REPOItemData(ItemClassification.filler,1,50,"Drone Shop Unlock"),
+    iname.zero_grav_drone: REPOItemData(ItemClassification.filler,1,51,"Drone Shop Unlock"),
+    iname.pocket_cart: REPOItemData(ItemClassification.filler,1,52,"Shop Unlock"),
+    iname.cart: REPOItemData(ItemClassification.filler,1,53,"Shop Unlock"),
+    iname.valuable_detector: REPOItemData(ItemClassification.filler,1,54,"Shop Unlock"),
+    iname.extraction_detector: REPOItemData(ItemClassification.filler,1,55,"Shop Unlock"),
+    iname.energy_crystal: REPOItemData(ItemClassification.filler,1,56,"Shop Unlock"),
+    iname.zero_grav_orb: REPOItemData(ItemClassification.filler,1,57,"Shop Unlock"),
+    iname.duck_bucket: REPOItemData(ItemClassification.filler,1,58,"Shop Unlock"),
+    iname.phase_bridge: REPOItemData(ItemClassification.filler,1,59,"Shop Unlock"),
 
     # ---- AP Function Items ----
     iname.shop_stock: REPOItemData(ItemClassification.progression,0,20,"Progressive Shop"),
--- worlds/repo/names/item_names.py
+++ worlds/repo/names/item_names.py
@@ -13,6 +13,60 @@ tumble_up = "Tumble Launch Upgrade"
 tumble_wings = "Tumble Wings Upgrade"
 crouch_rest = "Crouch Rest Upgrade"
 
+# Health
+progressive_health = "Progressive Health Pack Unlock"
+small_health = "Small Health Pack"
+medium_health = "Medium Health Pack"
+large_health = "Large Health Pack"
+
+# Melee Weapons
+baseball_bat = "Baseball Bat Unlock"
+frying_pan = "Frying Pan Unlock"
+sledge_hammer = "Sledge Hammer Unlock"
+sword = "Sword Unlock"
+inflatable_hammer = "Inflatable Hammer Unlock"
+prodzap = "Prodzap Unlock"
+
+# Ranged Weapons
+gun = "Gun Unlock"
+shotgun = "Shotgun Unlock"
+tranq_gun = "Tranq Gun Unlock"
+pulse_pistol = "Pulse Pistol Unlock"
+photon_blaster = "Photon Blaster Unlock"
+boltzap = "Boltzap Unlock"
+cart_cannon = "C.A.R.T. Cannon Unlock"
+cart_laser = "C.A.R.T. Laser Unlock"
+
+# Explosive Weapons
+grenade = "Grenade Unlock"
+shock_grenade = "Shock Grenade Unlock"
+stun_grenade = "Stun Grenade Unlock"
+human_grenade = "Human Grenade Unlock"
+duct_taped_grenade = "Duct Taped Grenade Unlock"
+shockwave_mine = "Shockwave Mine Unlock"
+stun_mine = "Stun Mine Unlock"
+explosive_mine = "Explosive Mine Unlock"
+rubber_duck = "Rubber Duck Unlock"
+
+# Drones
+recharge_drone = "Recharge Drone Unlock"
+indestructible_drone = "Indestructible Drone Unlock"
+roll_drone = "Roll Drone Unlock"
+feather_drone = "Feather Drone Unlock"
+zero_grav_drone = "Zero Gravity Drone Unlock"
+
+# CARTS
+pocket_cart = "Pocket C.A.R.T. Unlock"
+cart = "C.A.R.T. Unlock"
+
+# Misc
+valuable_detector = "Valuable Detector Unlock"
+extraction_detector = "Extraction Detector Unlock"
+energy_crystal = "Energy Crystal Unlock"
+zero_grav_orb = "Zero Gravity Orb Unlock"
+duck_bucket = "Duck Bucket Unlock"
+phase_bridge = "Phase Bridge Unlock"
+
 # ---- Level Type Items ----
 swiftbroom_lvl = "Swiftbroom Academy Level"
 headman_lvl = "Headman Manor Level"
