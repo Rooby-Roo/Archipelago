0f463999a22b8b75fce8a068198726dadc8e338e
RoobyRoo <thegreenrobby@gmail.com>
2025-08-18 13:48:45 -0600

    Add World Version: 0.2.0

--- worlds/frogmonster/__init__.py
+++ worlds/frogmonster/__init__.py
@@ -43,6 +43,7 @@ class FrogmonsterWorld(World):
     item_name_groups = item_name_groups
     web = FrogmonsterWebWorld()
 
+    apworld_version = (0, 2, 0)
     shuffled_bug_effects: dict[int, int]
     starter_gun: FrogmonsterItem
     starter_spell: FrogmonsterItem
