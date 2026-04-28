08649c78f064627cea598c2dca653238dfb12290
RoobyRoo <thegreenrobby@gmail.com>
2026-04-28 14:21:43 -0600

    Add reverse frog_bird connector
    Resolve's qwint's question on 3/29/26

--- worlds/animal_well/region_data.py
+++ worlds/animal_well/region_data.py
@@ -1093,6 +1093,8 @@ traversal_requirements: Dict[Union[lname, rname], Dict[Union[lname, rname], AWDa
         lname.key_frog_guard_room_east:
             AWData(AWType.location, [[iname.yoyo], [iname.bubble, iname.flute], 
                                      [iname.ball], [iname.flute_jump]]),
+        rname.frog_bird_after_yoyo_1:  # 
+            AWData(AWType.location),
         rname.frog_dark_room:  # yoyo to open the door, lantern to fall through the bird, or ball to hit buttons
             AWData(AWType.region, [[iname.yoyo], [iname.lantern], [iname.ball_trick_easy]]),
         rname.frog_ruby_egg_ledge:  # fall through a bird onto it
